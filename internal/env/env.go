a851f25d2d731e329be5d9ce62346a5f81574245
handlename <nagata@handlena.me>
2024-10-12 22:13:35 +0900

    refactor: no use of AWSC_ADDITIONAL_INFO

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,6 +4,5 @@ const (
 	EnvPrefix           = "AWSC"
 	EnvLogLevel         = EnvPrefix + "_LOG_LEVEL"
 	EnvConfigPath       = EnvPrefix + "_CONFIG_PATH"
-	EnvAdditionalInfo   = EnvPrefix + "_ADDITIONAL_INFO"
 	EnvDefaultConfigDir = "XDG_CONFIG_PATH"
 )
