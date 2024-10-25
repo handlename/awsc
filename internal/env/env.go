91d4d3039ded438cda866354ca048456a6e025db
handlename <nagata@handlena.me>
2024-10-25 16:08:23 +0900

    feat: show version

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,5 +4,6 @@ const (
 	EnvPrefix           = "AWSC"
 	EnvLogLevel         = EnvPrefix + "_LOG_LEVEL"
 	EnvConfigPath       = EnvPrefix + "_CONFIG_PATH"
+	EnvShowVersion      = EnvPrefix + "_SHOW_VERSION"
 	EnvDefaultConfigDir = "XDG_CONFIG_PATH"
 )
