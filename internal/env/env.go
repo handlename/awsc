200cd6c476dd7ebcb0715ef403663dfb1450be1b
NAGATA Hiroaki <nagata@handlena.me>
2024-12-24 17:43:40 +0900

    fix: XDG_CONFIG_HOME

--- internal/env/env.go
+++ internal/env/env.go
@@ -5,5 +5,5 @@ const (
 	EnvLogLevel         = EnvPrefix + "_LOG_LEVEL"
 	EnvConfigPath       = EnvPrefix + "_CONFIG_PATH"
 	EnvShowVersion      = EnvPrefix + "_SHOW_VERSION"
-	EnvDefaultConfigDir = "XDG_CONFIG_PATH"
+	EnvDefaultConfigDir = "XDG_CONFIG_HOME"
 )
